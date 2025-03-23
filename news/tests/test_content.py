68b7eac5a853752e4699d865b8e003a7a25ad09d
Korte0 <hawk1409@yandex.ru>
2025-03-23 20:14:07 +0700

    Test_logic

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -103,7 +103,7 @@ class TestDetailPage(TestCase):
     def test_anonymous_client_has_no_form(self):
         response = self.client.get(self.detail_url)
         self.assertNotIn('form', response.context)
-        
+
     def test_authorized_client_has_form(self):
         # Авторизуем клиент при помощи ранее созданного пользователя.
         self.client.force_login(self.author)
